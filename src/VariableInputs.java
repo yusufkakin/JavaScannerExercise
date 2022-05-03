8e0ca121a2d8a7f65c842ffdbe30b3674b75d494
Yusuf Akin <kutluhanakin@gmail.com>
2022-05-03 09:04:41 -0400

    prettier

--- src/VariableInputs.java
+++ src/VariableInputs.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class VariableInputs {
     public static void main(String[] args) {
-        System.out.println("Enter a string of text:");
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string of text:");
         String text = scanner.nextLine();
         System.out.println("Enter an integer:");
         int intValue = scanner.nextInt();
-        scanner.nextLine();
         System.out.println("Enter a double:");
-        double doubValue = scanner.nextDouble();
+        double doubleValue = scanner.nextDouble();
         System.out.println("Enter a boolean value:");
         boolean boolValue = scanner.nextBoolean();
 
         System.out.println("Your string is "+ text);
         System.out.println("Your integer is " + intValue);
-        System.out.println("Your double is " + doubValue);
+        System.out.println("Your double is " + doubleValue);
         System.out.println("Your boolean is " + boolValue);
 
     }
